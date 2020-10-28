5afdc61b31c23a77da130fd4d5302c6c7debc172
meiwek7 <meiwek7@mail.ru>
2020-10-28 16:06:43 +0200

    TestCommit

--- homeWork/001 - 19_10_2020/script.js
+++ homeWork/001 - 19_10_2020/script.js
@@ -1,9 +1,9 @@
 // 1) Запросите у пользователя его имя и выведите в ответ:
 // «Привет, его имя!».
 
-// let name = prompt("Enter your name");
+let name = prompt("Enter your name");
 
-// alert("Hello, "+name+"!");
+alert("Hello, "+name+"!");
 
 // 2) Запросите у пользователя год его рождения, посчитайте,
 // сколько ему лет и выведите результат. Текущий год укажите
@@ -11,16 +11,16 @@
 
 // const CUR_YEAR = 2020;
 
-// let userBirthDate = prompt("Enter the year of your birth");
+let userBirthDate = prompt("Enter the year of your birth");
 
-// alert("Your age is: "+(CUR_YEAR-userBirthDate));
+alert("Your age is: "+(CUR_YEAR-userBirthDate));
 
 // 3) Запросите у пользователя длину стороны квадрата и вы-
 // ведите периметр такого квадрата.
 
-// let side = prompt("Enter the side of a square:");
+let side = prompt("Enter the side of a square:");
 
-// alert("Perimeter is: "+(side*4));
+alert("Perimeter is: "+(side*4));
 
 // 4) Запросите у пользователя радиус окружности и выведите
 // площадь такой окружности.
